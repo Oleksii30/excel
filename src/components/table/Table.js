fca870386a627d4ec0982e68afeb666948fd0796
Oleksii30 <polyanovskyyol@gmail.com>
2020-09-05 21:41:50 +0300

    finished resize of the table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,29 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
+// import {$} from '@core/dom'
+import {resizeTable} from './table.resize'
+import {shouldResize} from './table.functions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown', 'mouseup']
+    })
+  }
+
   toHTML() {
     return createTable(20)
   }   
+  
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeTable(event, this.$root)
+    }
+  }
+  
+  onMouseup() {
+    
+  } 
 }
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,40 @@
+import {$} from '@core/dom'
+
+export function resizeTable(event, $root) {
+  const $resizer = $(event.target)      
+  const $parent = $resizer.closest('[data-type="resizable"]')
+  const type = event.target.dataset.resize            
+  const coords = $parent.getCoords()
+  const colAttribute = $parent.dataset.colnumber      
+  let value
+  const sideProp = type === 'col'? 'bottom' : 'right'     
+  $resizer.css({opacity: 1, [sideProp]: -5000 + 'px'})     
+             
+  document.onmousemove = e => {
+    let delta = null
+           
+    if (type === 'col') {   
+      delta = e.pageX - coords.right   
+      value = coords.width + delta
+      $resizer.css({right: -delta + 'px'})          
+    } else {
+      delta = e.pageY - coords.bottom       
+      value = coords.height + delta
+      $resizer.css({bottom: -delta + 'px'})             
+    }       
+  }
+  document.onmouseup = () => { 
+    if (type === 'col') {
+      $parent.css({width: value + 'px'})
+      const cellsInColumn = Array.from($root.findAll(`[data-colNumber="${colAttribute}"]`))
+      cellsInColumn.shift()      
+      cellsInColumn
+          .map(el => $(el))   
+          .forEach(el => el.style.width = value + 'px')                   
+    } else {
+      $parent.css({height: value + 'px'})         
+    }  
+    $resizer.css({opacity: 0, bottom: 0, right: 0})
+    document.onmousemove = null
+  }
+} 
